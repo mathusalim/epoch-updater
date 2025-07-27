3815f574c65a85b5118abcd7ecf334d01bae6391
Rezo Getiashvili <r1getiashvili@gmail.com>
2025-07-27 11:25:13 +0200

    declassifying settings

--- src/ts/backend/settings.ts
+++ src/ts/backend/settings.ts
@@ -4,69 +4,55 @@ import Store, { Schema } from 'electron-store';
  * Create our Settings Structure with explicit types for TS.
  */
 interface SettingStructure {
-    clientDirectory: string;
-    environment: string;
-    key: string;
-    cdn: boolean;
-    cdnProvider: string;
+  clientDirectory: string;
+  environment: string;
+  key: string;
+  cdn: boolean;
+  cdnProvider: string;
 }
 
 /**
  * Actually create the Settings Store Structure and assign defaults.
  */
 const schema: Schema<SettingStructure> = {
-    clientDirectory: {
-        type: 'string',
-        default: '',
-    },
-    environment: {
-        type: 'string',
-        default: 'production',
-    },
-    key: {
-        type: 'string',
-        default: '',
-    },
-    cdn: {
-        type: 'boolean',
-        default: true,
-    },
-    cdnProvider: {
-        type: 'string',
-        default: 'cloudflare',
-    }
+  clientDirectory: {
+    type: 'string',
+    default: '',
+  },
+  environment: {
+    type: 'string',
+    default: 'production',
+  },
+  key: {
+    type: 'string',
+    default: '',
+  },
+  cdn: {
+    type: 'boolean',
+    default: true,
+  },
+  cdnProvider: {
+    type: 'string',
+    default: 'cloudflare',
+  },
 };
 
 /**
- * Create Wrapper Class.
+ * return an object with a storage method that returns the electron-store instance.
  */
-export class Settings {
-    private store: Store<SettingStructure>;
-
-    constructor() {
-        this.store = new Store<SettingStructure>({
-            migrations: {
-                '1.1.0': store => {
-                    store.set('cdnProvider', 'cloudflare');
-                },
-                '1.0.13': store => {
-                    store.set('cdn', true);
-                },
-            },
-            schema: schema,
-        });
-    }
-
-    /**
-     * Reference to electron-store.
-     * @returns The electron-store instance.
-     */
-    storage(): Store<SettingStructure> {
-        return this.store;
-    }
-}
-
-/**
- * Create an Instance of our Wrapper.
- */
-export const SettingsManager = new Settings();
+export const settingsManager = (() => {
+  const store = new Store<SettingStructure>({
+    migrations: {
+      '1.1.0': (store) => {
+        store.set('cdnProvider', 'cloudflare');
+      },
+      '1.0.13': (store) => {
+        store.set('cdn', true);
+      },
+    },
+    schema: schema,
+  });
+  return {
+    storage: (): Store<SettingStructure> => store,
+  };
+})();
